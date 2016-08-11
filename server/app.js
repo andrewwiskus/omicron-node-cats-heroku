6ca2d86bac4329eb26f9e64801cc3fe4ef1d49d5
Andrew Wiskus <MyWork@Andrews-MacBook-Pro.local>
2016-08-11 14:40:32 -0500

    work

--- server/app.js
+++ server/app.js
@@ -2,31 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var catRoute = require('./routes/cats');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-// data array
-var cats = [{name: 'Luna', trait: 'Fiesty'}];
 
 // Routes
-app.get('/cat', function(req, res) {
-  res.send(cats);
-});
-
-app.post('/new', function(req, res) {
-  console.log('request: ', req.body);
-  cats.push(req.body);
-  res.sendStatus(201);
-});
+app.use('/cats', catRoute);
 
 // serve static files
 app.get('/*', function(req, res) {
-  var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(__dirname, './public', file));
+    var file = req.params[0] || '/views/index.html';
+    res.sendFile(path.join(__dirname, './public', file));
 });
 
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function() {
-  console.log('server is running on port ', app.get('port'));
+    console.log('server is running on port ', app.get('port'));
 });
